872b22c595b0c63ac87f443ff910ac2ec9610784
jonathanng1122 <jonathanng1122@gmail.com>
2022-01-03 18:22:54 -0600

    started updateuser changes

---
+++ src/dto/UpdateUserDto.ts
@@ -0,0 +1,20 @@
+import { IsArray, IsEthereumAddress, IsNumber, IsString, IsUrl } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsEthereumAddress()
+  address: string;
+  @IsString()
+  bio: string;
+  @IsNumber()
+  deliveryTime: number;
+  @IsArray()
+  demos: string[];
+  @IsUrl()
+  profilePicture: string;
+  @IsUrl()
+  tweetUrl: string;
+  @IsString()
+  userName: string;
+  @IsNumber()
+  price: number;
+}
